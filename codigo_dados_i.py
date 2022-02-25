3931bed3afd66a89b698b4efd09713226d7d227f
LaioLuis <laioluis11@gmail.com>
2022-02-25 16:48:06 -0300

    correção dos valores do eixo x, separando "tri" do ano

--- codigo_dados_i.py
+++ codigo_dados_i.py
@@ -22,11 +22,11 @@ graf3 = grafico3[['V']].reset_index(drop=True)
 graf4 = grafico4[['V']].reset_index(drop=True)
 graf5 = grafico5[['V']].reset_index(drop=True)
 
-graf1 = graf1.drop([33,34,35,36,37,38])
-graf2 = graf2.drop([33,34,35,36,37,38])
-graf3 = graf3.drop([33,34,35,36,37,38])
-graf4 = graf4.drop([33,34,35,36,37,38])
-graf5 = graf5.drop([33,34,35,36,37,38])
+graf1 = graf1.drop([33,34,35,36,37,38,39])
+graf2 = graf2.drop([33,34,35,36,37,38,39])
+graf3 = graf3.drop([33,34,35,36,37,38,39])
+graf4 = graf4.drop([33,34,35,36,37,38,39])
+graf5 = graf5.drop([33,34,35,36,37,38,39])
 
 graf1.rename(columns={'D3N': 'trimestre', 'V': '14-17'}, inplace=True)
 graf2.rename(columns={'V': '18-24'}, inplace=True)
@@ -36,10 +36,10 @@ graf5.rename(columns={'V': '60+'}, inplace=True)
 graf1['trimestre'] = graf1['trimestre'].replace(['1º trimestre 2012','2º trimestre 2012','3º trimestre 2012','4º trimestre 2012','1º trimestre 2013','2º trimestre 2013','3º trimestre 2013',\
     '4º trimestre 2013','1º trimestre 2014','2º trimestre 2014','3º trimestre 2014','4º trimestre 2014','1º trimestre 2015','2º trimestre 2015','3º trimestre 2015','4º trimestre 2015',\
         '1º trimestre 2016','2º trimestre 2016','3º trimestre 2016','4º trimestre 2016','1º trimestre 2017','2º trimestre 2017','3º trimestre 2017','4º trimestre 2017','1º trimestre 2018',\
-            '2º trimestre 2018','3º trimestre 2018','4º trimestre 2018','1º trimestre 2019','2º trimestre 2019','3º trimestre 2019','4º trimestre 2019','1º trimestre 2020'], ['1ºtri2012','2ºtri2012','3ºtri2012','4ºtri2012','1ºtri2013','2ºtri2013','3ºtri2013',\
-    '4ºtri2013','1ºtri2014','2ºtri2014','3ºtri2014','4ºtri2014','1ºtri2015','2ºtri2015','3ºtri2015','4ºtri2015',\
-        '1ºtri2016','2ºtri2016','3ºtri2016','4ºtri2016','1ºtri2017','2ºtri2017','3ºtri2017','4ºtri2017','1ºtri2018',\
-            '2ºtri2018','3ºtri2018','4ºtri2018','1ºtri2019','2ºtri2019','3ºtri2019','4ºtri2019','1ºtri2020'])
+            '2º trimestre 2018','3º trimestre 2018','4º trimestre 2018','1º trimestre 2019','2º trimestre 2019','3º trimestre 2019','4º trimestre 2019','1º trimestre 2020'], ['1ºtri/2012','2ºtri/2012','3ºtri/2012','4ºtri/2012','1ºtri/2013','2ºtri/2013','3ºtri/2013',\
+    '4ºtri/2013','1ºtri/2014','2ºtri/2014','3ºtri/2014','4ºtri/2014','1ºtri/2015','2ºtri/2015','3ºtri/2015','4ºtri/2015',\
+        '1ºtri/2016','2ºtri/2016','3ºtri/2016','4ºtri/2016','1ºtri/2017','2ºtri/2017','3ºtri/2017','4ºtri/2017','1ºtri/2018',\
+            '2ºtri/2018','3ºtri/2018','4ºtri/2018','1ºtri/2019','2ºtri/2019','3ºtri/2019','4ºtri/2019','1ºtri/2020'])
 graf = pd.concat([graf1,graf2,graf3,graf4,graf5], axis=1)
 print(graf)
 graf.to_csv('E:/Área de trabalho/Códigos/R/rs_dados_trabalhoemprego_desemprego/data/recossa_desemprego_i.csv', index=False)
